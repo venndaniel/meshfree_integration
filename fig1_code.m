b4700abec1713f740d6c16eb549bc495102c1fae
venndaniel <daniel_venn@sfu.ca>
2025-10-15 16:34:55 -0700

    Additional comments

--- fig1_code.m
+++ fig1_code.m
@@ -1,7 +1,10 @@
+% Script to generate Figure 1, with data generated using test4p1p1.m
+
 set(groot,'defaulttextinterpreter','latex'); 
 set(groot, 'defaultAxesTickLabelInterpreter','latex');  
 set(groot, 'defaultLegendInterpreter','latex')
 
+% Number of points tested for q = 7/3
 n73 = [400
 800
 1200
@@ -9,9 +12,9 @@ n73 = [400
 2000
 2400
 2800
-
 ];
 
+% errors for q = 7/3
 err73 = [2.11E-02
 5.65E-03
 1.01E-03
@@ -19,10 +22,9 @@ err73 = [2.11E-02
 7.43E-05
 7.94E-05
 1.37E-05
-
-
 ];
 
+% Number of points tested for q = 10/3
 n103 = [400
 800
 1200
@@ -30,9 +32,10 @@ n103 = [400
 2000
 2400
 2800
-
 ];
 
+
+% errors for q = 10/3
 err103 = [9.21E-03
 1.77E-03
 4.17E-04
@@ -40,10 +43,10 @@ err103 = [9.21E-03
 7.42E-05
 1.19E-05
 1.75E-05
-
 ];
 
-nfem = [400
+% Number of points tested for triangulation and simple average
+ntri= [400
 800
 1600
 3200
@@ -52,7 +55,9 @@ nfem = [400
 25600
 51200
 ];
-errfem =[
+
+% error for triangulation
+errtri =[
  6.50E-02
 5.04E-02
 3.63E-03
@@ -61,10 +66,10 @@ errfem =[
 4.64E-04
 2.22E-04
 1.14E-04
-
 ];
 
-errmont = [2.16E-02
+% error for simple average
+erravg = [2.16E-02
 3.02E-02
 4.11E-04
 1.02E-02
@@ -72,14 +77,13 @@ errmont = [2.16E-02
 1.46E-02
 1.24E-02
 1.56E-02
-
 ];
 
 loglog(n73, err73, 'o-');
 hold on;
 plot(n103, err103, 'o-');
-plot(nfem, errfem, 'o-');
-plot(nfem, errmont, 'o-');
+plot(ntri, errtri, 'o-');
+plot(ntri, erravg, 'o-');
 
 legend('Meshfree: $q=7/3$', 'Meshfree: $q=10/3$', 'Triangulation', 'Point Cloud Average');
 grid on;
